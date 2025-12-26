153580883fe29b3f8fc5419b44a1bb125fa77b33
Prateek Dev <pdprateek146@gmail.com>
2025-12-26 16:42:06 +0530

    Making Request in the middle

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -30,12 +30,19 @@ const Requests = () => {
         );
     }
   return (
-    <div className='justify-center my-10'>
+    <div className='flex flex-col items-center my-10'>
         <h1 className='text-bold text-2xl'>Requests</h1>
        {store.map((connection) => (
-        <div className='m-4 p-4 bg-base-200 rounded-lg' key={connection.fromUser._id}>
+        <>
+        <div className='flex gap-8 m-4 p-4 mx-auto justify-between items-center bg-base-200 rounded-lg w-1/2' key={connection.fromUser._id}>
             <h2>{connection.fromUser.firstName} {connection.fromUser.lastName}</h2>
+            <div className='flex gap-4 mt-2'>
+            <button className="btn btn-primary">Reject</button>
+            <button className="btn btn-secondary">Accept</button>
+            </div>
         </div>
+        </>
+        
        ))}
     </div>
   );
