7a0cdaa4ffcf6b7a7b5141d35d9195559a175312
Prateek Dev <pdprateek146@gmail.com>
2025-12-27 13:20:32 +0530

    Sign Up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,6 +11,27 @@ const Login = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [error,setError] = useState("");
+    const [firstName,setFirstName] = useState("");
+    const [lastName,setLastName] = useState("");
+    const [age,setAge] = useState("");
+    const [gender,setGender] = useState("");
+    const [skills,setSkills] = useState("");
+    const [about,setAbout] = useState("");
+    const [photo,setPhoto] = useState("");
+    const [isLoginMode,setIsLoginMode] = useState(true); // true for login, false for signup
+
+    const handlesignup =  async () => {
+      try{
+          const response = await axios.post(BASE_URL+'signup',{firstName,lastName,age,gender,skills,about,photo,email,password},{withCredentials:true});
+          console.log("Signup successful", response);
+          dispatch(addUser(response.data.data));
+          navigate('/profile');
+      }
+      catch(error){
+        setError(error.response?.data || "Signup failed");
+        console.error("Signup failed", error);
+      }
+    }
 
     const handlelogin = async () => {
         try {
@@ -29,10 +50,11 @@ const Login = () => {
         }
     }
   return (
+    <>
     <div className='flex justify-center my-10'>
     <div className="card bg-base-300 text-primary-content w-96">
   <div className="card-body">
-    <h2 className="card-title">Login</h2>
+    <h2 className="card-title">{isLoginMode ? "Login" : "Sign Up"}</h2>
     <div>
     <fieldset className="fieldset">
   <legend className="fieldset-legend">Email Id</legend>
@@ -43,13 +65,48 @@ const Login = () => {
   <input type="password" value={password} className="input" placeholder="Type here" onChange={(e) => setPassword(e.target.value)}/>
     </fieldset>
     </div>
+    {!isLoginMode &&<div>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">FirstName</legend>
+      <input type="text" value = {firstName} className="input" placeholder="Type here"  onChange={(e) => setFirstName(e.target.value)}/>
+    </fieldset>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">LastName</legend>
+      <input type="text" value = {lastName} className="input" placeholder="Type here"  onChange={(e) => setLastName(e.target.value)}/>
+    </fieldset>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">Age</legend>
+      <input type="text" value = {age} className="input" placeholder="Type here"  onChange={(e) => setAge(e.target.value)}/>
+    </fieldset>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">Gender</legend>
+      <input type="text" value = {gender} className="input" placeholder="Type here"  onChange={(e) => setGender(e.target.value)}/>
+    </fieldset>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">Skills</legend>
+      <input type="text" value = {skills} className="input" placeholder="Type here"  onChange={(e) => setSkills(e.target.value)}/>
+    </fieldset>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">About</legend>
+      <input type="text" value = {about} className="input" placeholder="Type here"  onChange={(e) => setAbout(e.target.value)}/>
+    </fieldset>
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">Photo</legend>
+      <input type="text" value = {photo} className="input" placeholder="Type here"  onChange={(e) => setPhoto(e.target.value)}/>
+    </fieldset>
+    </div>
+  }
     <p className="text-red-50">{error}</p>
     <div className="card-actions justify-center py-2">
-      <button className="btn" onClick={handlelogin}>Log In</button>
+      <button className="btn" onClick={isLoginMode ? handlelogin : handlesignup}>{isLoginMode ? "Log In":"Sign Up"}</button>
     </div>
+    <p className="text-red-50 cursor-pointer"  onClick={() => setIsLoginMode(value => !value)}>{isLoginMode ? "New User? Signup here":"Existing User? Login here"}</p>
   </div>
 </div>
-</div>
+    </div>
+
+
+    </>
   );
 };
 
